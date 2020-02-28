4dc3e4caef313691f64ed9df0b6314db0e9d83e2
Felipe Godoy C <fgodoy6@externos.bancoestado.cl>
2020-02-28 12:27:12 -0300

    configuracion eslint y prettier, hacer npm i

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,18 +1,18 @@
 // Declara variables
-const express = require('express')
+const express = require("express");
 const router = express.Router();
 
 const base = process.env.NODE_ENV || "/bff/v1/se-bff-empresas";
 // const base = process.env.NODE_ENV || "/api/v1/bff/se-bff-empresas";
 
-const gestorContenidosRoutes = require('./gestorContenido.routes');
-const healthcheckRoutes = require('./healthcheck.routes');
-const paisesRoutes = require('./ejemplo-paises.routes');
+const gestorContenidosRoutes = require("./gestorContenido.routes");
+const healthcheckRoutes = require("./healthcheck.routes");
+const paisesRoutes = require("./ejemplo-paises.routes");
 
-router.use('/gestorContenido', gestorContenidosRoutes);
-router.use('/healthcheck', healthcheckRoutes);
-router.use('/paises', paisesRoutes);
+router.use("/gestorContenido", gestorContenidosRoutes);
+router.use("/healthcheck", healthcheckRoutes);
+router.use("/paises", paisesRoutes);
 
-module.exports = (app) => {
-    app.use(base, router);
-}
+module.exports = app => {
+  app.use(base, router);
+};
