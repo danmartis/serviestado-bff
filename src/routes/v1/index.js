ef7a0dc84c81890be84bcc828111d49739dc4376
Felipe Godoy C <fgodoy6@externos.bancoestado.cl>
2020-03-02 10:08:25 -0300

    fix router

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -2,8 +2,6 @@
 const express = require("express");
 const router = express.Router();
 
-const base = process.env.NODE_ENV || "/bff/v1/se-bff-empresas";
-// const base = process.env.NODE_ENV || "/api/v1/bff/se-bff-empresas";
 
 const gestorContenidosRoutes = require("./gestorContenido.routes");
 const healthcheckRoutes = require("./healthcheck.routes");
@@ -13,6 +11,4 @@ router.use("/gestorContenido", gestorContenidosRoutes);
 router.use("/healthcheck", healthcheckRoutes);
 router.use("/paises", paisesRoutes);
 
-module.exports = app => {
-  app.use(base, router);
-};
+module.exports = router;
