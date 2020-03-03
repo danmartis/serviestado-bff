0a1b40ee2d3ae1864c49b44eff8a80b6d6d300f9
Felipe Godoy C <fgodoy6@externos.bancoestado.cl>
2020-03-03 15:54:56 -0300

    fix checkObject para que todos los campos sean requeridos

--- src/utils/data.utils.js
+++ src/utils/data.utils.js
@@ -1,4 +1,14 @@
 export const checkObject = (obj, arr) => {
+    if(obj) {
+        const objectKeys = Object.keys(obj);
+        for (const key of arr) {
+            if(!objectKeys.includes(key)) return false;            
+        }
+        return true;
+    }
+}
+
+export const checkObjectTest = (obj,arr) => {
     if(obj) {return Object.keys(obj).every(e => arr.includes(e));}
     else { return false; }
 }
