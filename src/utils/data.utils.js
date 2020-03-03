b2da7f21352d7b3228cd6c0a6e88e73909e1d6b5
Felipe Godoy C <fgodoy6@externos.bancoestado.cl>
2020-03-03 16:11:17 -0300

    add validaciones a utils

--- src/utils/data.utils.js
+++ src/utils/data.utils.js
@@ -1,11 +1,12 @@
 export const checkObject = (obj, arr) => {
-    if(obj) {
-        const objectKeys = Object.keys(obj);
-        for (const key of arr) {
-            if(!objectKeys.includes(key)) return false;            
-        }
-        return true;
+    if(!obj) throw Error("Objeto o propiedad ingresada no esta definida");
+    if(typeof obj != 'object') throw Error("La variable ingresada no es objeto");
+    if(!Array.isArray(arr)) throw Error("Las llaves deben estar dentro de un array");
+    
+    for (const key of arr) {
+        if(!obj.hasOwnProperty(key)) return false;            
     }
+    return true;
 }
 
 export const checkObjectTest = (obj,arr) => {
