0472a3902f9c7de9b938c37dc6ec6c18d3eda52c
Felipe Godoy C <fgodoy6@externos.bancoestado.cl>
2020-03-03 15:54:20 -0300

    fix controlador y reenvio de data

--- src/app/controllers/v1/registro.controller.js
+++ src/app/controllers/v1/registro.controller.js
@@ -13,21 +13,21 @@ const formTest = {
         "phone" : "+56123456789",
         "position" : "Big Boss"
     }
-}
-const keysDatosEmpresa = ["business_name", "rut"]
-const keysDatosContacto = ["name","last_name","email","phone","position"]
+};
+const keysBody = ["business_name", "rut","name","last_name","email","phone","position"];
 
 export const registroBiller = (req,res) => {
     new Promise( (resolve, reject) => {
         let body = req.body;
-        if(!checkObject(body.DATOS_EMPRESA, keysDatosEmpresa) | !checkObject(body.DATOS_CONTACTOS, keysDatosContacto)){
-            reject("Data no valida o incompleta")
+        if( !checkObject(body, keysBody)){
+            reject(new Error("Data no valida o incompleta"));
         } else {
             resolve(body);
-        }        
+        }
     })
+    .then( data => sendRegistroBillerMS(data))
     .then( data => res.status(200).json( {ok: true, data } ))
-    .catch( err => res.status(400).json( {ok: false, error: err}));
+    .catch( err => res.status(400).json( {ok: false, error: err.message}));
 }
 
 export const getBiller = (req,res) => {
--- src/app/services/v1/registro-biller-ms.services.js
+++ src/app/services/v1/registro-biller-ms.services.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
 export const sendRegistroBillerMS = (data) => {
-    return axios.post('url-ms', data);
+    // return axios.post('url-ms', data);
+    console.log("Enviando a ms-registro-biller");
+    return data;
 }
