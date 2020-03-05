4c6bb626e05a2309ca01073005ff4a5d17946125
Felipe Godoy C <fgodoy6@externos.bancoestado.cl>
2020-03-05 17:48:52 -0300

    se elimina ejemplo paises

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -4,12 +4,10 @@ const router = Router();
 
 import gestorContenidosRoutes from "./gestorContenido.routes";
 import healthcheckRoutes from "./healthcheck.routes";
-import paisesRoutes from "./ejemplo-paises.routes";
 import registroBillerRoutes from "./registro.routes";
 
 router.use("/gestorContenido", gestorContenidosRoutes);
 router.use("/healthcheck", healthcheckRoutes);
-router.use("/paises", paisesRoutes);
 router.use("/registro-biller", registroBillerRoutes);
 
 export default router;
