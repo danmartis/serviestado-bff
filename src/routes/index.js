ef3eca104dbc397ec13f22be822f783b204ac813
Felipe Godoy C <fgodoy6@externos.bancoestado.cl>
2020-03-05 17:48:41 -0300

    se elimina uso de variables de entorno por problemas con jest

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,10 +4,10 @@ const router = Router();
 
 import v1 from "./v1";
 
-const base = process.env.NODE_ENV || "/bff/se-bff-empresas";
+const base = "/bff/se-bff-empresas";
 
 router.use('/v1', v1);
 
 export default app => {
-  app.use(base, router);
-};
+    app.use(base, router);
+  };
