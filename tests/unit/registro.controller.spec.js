77f65556cf1970fde45a2e46b65a8e3178f8d69b
Felipe Godoy C <fgodoy6@externos.bancoestado.cl>
2020-03-05 17:49:05 -0300

    add pruebas unitarias jest

---
+++ tests/unit/registro.controller.spec.js
@@ -0,0 +1,38 @@
+import supertest from 'supertest';
+import '@babel/polyfill';
+import app from '../../src/app';
+
+const data = {
+    "business_name" : "ENTEL S.A.",
+    "rut" : "11.111.111-1",
+    "name" : "Pedro",
+    "last_name" : "Engel",
+    "email" : "pedrito@engel.cl",
+    "phone" : "+56123456789",
+    "position" : "Big Boss"
+}
+
+const request = supertest(app);
+describe('Test registro controller', () => {
+
+    test('Prueba registro', async () => {
+        const res = await request
+                        .post('/bff/se-bff-empresas/v1/registro-biller')
+                        .send(data);
+        expect(res.statusCode).toEqual(200);
+    });
+
+    test('Prueba registro error', async () => {
+        const res = await request
+                        .post('/bff/se-bff-empresas/v1/registro-biller')
+                        .send({});
+        expect(res.statusCode).toEqual(400);
+    });
+
+    test('Get biller', async() => {
+        const res = await request
+                            .get('/bff/se-bff-empresas/v1/registro-biller/123')
+        expect(res.statusCode).toEqual(200);
+    })
+
+});
