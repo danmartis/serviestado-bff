0b0bf1ece2e0a325f0a8483021d51d041382ed6e
Felipe Godoy C <fgodoy6@externos.bancoestado.cl>
2020-03-09 11:22:33 -0300

    add servicio login

--- src/app/services/v1/registro-biller-ms.services.js
+++ src/app/services/v1/registro-biller-ms.services.js
@@ -1,7 +1,37 @@
 import axios from "axios";
 
-export const sendRegistroBillerMS = (data) => {
+const users = [
+    {
+        email: 'cliente@cliente.cl',
+        rut: '11111111-1',
+        password: 'RC123456',
+        changePassword: true
+    },
+    {
+        email: 'entel@cliente.cl',
+        rut: '11111111-2',
+        password: '12345678',
+        changePassword: false
+    },
+];
+
+const indexBy = (array, key) => array.reduce((acc, val) => { acc[val[key]] = val; return acc }, {});
+
+export const sendRegistroBillerMS = (dataIn) => {
     // return axios.post('url-ms', data);
-    console.log("Enviando a ms-registro-biller");
-    return data;
+    return dataIn;
+}
+
+export const loginBillerMS = (dataIn) => {
+    // return axios.post('url-ms', dataIn);
+    return new Promise( (resolve, reject) => {
+        const usersIndexed = indexBy(users, 'rut');
+        const user = usersIndexed[dataIn.rut];
+        if( !user ) reject("Usuario no encontrado");
+        if( user.email === dataIn.email && user.password === dataIn.password){
+            resolve(user);
+        } else {
+            reject("Usuario o contraseña incorrecto");
+        }
+    })
 }
