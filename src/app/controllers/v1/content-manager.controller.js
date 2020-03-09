7622f95129262cd9375e3d3934a74e53f7530f5d
frank.mujica <frank.mujica@siigroup.cl>
2020-03-09 14:39:16 -0300

    testing content manager

--- src/app/controllers/v1/content-manager.controller.js
+++ src/app/controllers/v1/content-manager.controller.js
@@ -3,11 +3,10 @@ import questions from "../../mock-data/help-questions.json";
 import questionDetails from '../../mock-data/help-question-details.json';
 import videos from '../../mock-data/help-videos.json';
 
-export const controllerLandingContent = (req, res) => {
+export const controllerLandingContent = async (req, res) => {
   try {
     return res.status(200).send(landing);
   } catch (error) {
-    console.log('Error:', error);
     return res.status(500).json(`Error: ${error}`);
   }
 }
--- tests/unit/gestor-contenido.controller.spec.js
+++ tests/unit/gestor-contenido.controller.spec.js
@@ -4,8 +4,29 @@ import app from '../../src/app';
 
 const request = supertest(app);
 describe('Test gestor contenido', () => {
-    test('Get', async () => {
-        const res = await request.get('/bff/se-bff-empresas/v1/gestorContenido')
-        expect(res.statusCode).toBe(200);
-    })
+
+    test('Get landing content', async () => {   
+        const res = await request
+        .get('/bff/se-bff-empresas/v1/gestor-contenido/landing');
+        expect(res.statusCode).toEqual(200);
+    });
+
+    test('Get questions content', async () => {
+        const res = await request   
+        .get('/bff/se-bff-empresas/v1/gestor-contenido/questions')
+        expect(res.statusCode).toEqual(200);
+    });
+
+    test('Get question details content', async () => {
+        const res = await request
+        .get('/bff/se-bff-empresas/v1/gestor-contenido/question-details');
+        expect(res.statusCode).toEqual(200);
+    });
+
+    test('Get videos content', async () => {
+        const res = await request   
+        .get('/bff/se-bff-empresas/v1/gestor-contenido/videos');
+        expect(res.statusCode).toEqual(200);
+    });
+
 });
