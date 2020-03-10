5fb8a799195e5619010b07141ec7b53f9c078e69
frank.mujica <frank.mujica@siigroup.cl>
2020-03-10 11:11:45 -0300

    Add tests of content manager with 100% coverage

--- src/app/controllers/v1/content-manager.controller.js
+++ src/app/controllers/v1/content-manager.controller.js
@@ -3,34 +3,34 @@ import questions from "../../mock-data/help-questions.json";
 import questionDetails from '../../mock-data/help-question-details.json';
 import videos from '../../mock-data/help-videos.json';
 
-export const controllerLandingContent = async (req, res) => {
+export const controllerLandingContent = (req, res) => {
   try {
     return res.status(200).send(landing);
-  } catch (error) {
-    return res.status(500).json(`Error: ${error}`);
+  } catch (err) {
+    throw new Error('Error: ', err.message);
   }
 }
 
 export const controllerQuestionsContent = (req, res) => {
   try {
     return res.status(200).send(questions);
-  } catch (error) {
-    return res.status(500).json(`Error: ${error}`);
+  } catch (err) {
+    throw new Error('Error: ', err.message);
   }
 }
 
 export const controllerQuestionDetailsContent = (req, res) => {
   try {
     return res.status(200).send(questionDetails);
-  } catch (error) {
-    return res.status(500).json(`Error: ${error}`);
+  } catch (err) {
+    throw new Error('Error: ', err.message);
   }
 }
 
 export const controllerVideosContent = (req, res) => {
   try {
     return res.status(200).send(videos);
-  } catch (error) {
-    return res.status(500).json(`Error: ${error}`);
+  } catch (err) {
+    throw new Error('Error: ', err.message);
   }
 }
