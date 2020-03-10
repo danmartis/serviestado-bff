2de01ba2aab9cd13e2a37b4b8db431fd180fcaaa
Felipe Godoy C <fgodoy6@externos.bancoestado.cl>
2020-03-10 11:33:30 -0300

    add mensajes de respuesta

--- src/app/controllers/v1/login-biller.controller.js
+++ src/app/controllers/v1/login-biller.controller.js
@@ -1,9 +1,17 @@
 import { loginBillerMS } from "../../services/v1/registro-biller-ms.services";
-import { mensajeSalida } from "../../services/v1/mensaje-salida.service";
+import { mensajeSalida, CODE_MESSAGE_OK, CODE_RESP_OK, CODE_RESP_BAD_REQUEST, CODE_MESSAGE_ERROR, LOGIN_BILLER_RESP } from "../../services/v1/mensaje-salida.service";
 
 export const loginBiller = (req,res) => {
     const body = req.body;
     loginBillerMS(body)
-        .then( data => res.status(200).json(mensajeSalida(200,"Exito", { ...data })))
-        .catch( err => res.status(400).json(mensajeSalida(200,"Error", { message: err})))
+        .then( data => 
+            res.status(CODE_RESP_OK)
+                    .json( mensajeSalida(CODE_MESSAGE_OK,
+                                        LOGIN_BILLER_RESP.SUCCESS, 
+                                        { ...data })) )
+        .catch( err => 
+            res.status(CODE_RESP_BAD_REQUEST)
+                    .json( mensajeSalida(CODE_MESSAGE_ERROR,
+                                        LOGIN_BILLER_RESP.ERROR, 
+                                        { message: err })) )
 }
--- src/app/services/v1/mensaje-salida.service.js
+++ src/app/services/v1/mensaje-salida.service.js
@@ -1,1 +1,29 @@
 export const mensajeSalida = (codigo, mensaje, data) => ({codigo,mensaje,data});
+// CÓDIGO DE RESPUESTA
+export const CODE_RESP_OK = 200;
+export const CODE_RESP_CREATED = 201;
+export const CODE_RESP_BAD_REQUEST = 400;
+export const CODE_RESP_NOT_FOUND = 404;
+export const CODE_RESP_UNAUTHORIZED = 401;
+export const CODE_RESP_METHOD_NOT_ALLOWED = 405;
+export const CODE_RESP_CONFLICT = 409;
+export const CODE_RESP_INTERNAL_SERVER_ERROR = 500;
+// CÓDIGO MENSAJES
+export const CODE_MESSAGE_OK = "OK";
+export const CODE_MESSAGE_ERROR = "ERROR";
+// DESCRIPCION ERRORES
+export const REGISTRO_BILLER_RESP = {
+    SUCCESS: "Registro exitoso",
+    ERROR: "Registro fallido, verifique informacion ingresada"
+}
+export const LOGIN_BILLER_RESP = {
+    SUCCESS: "Inicio de sesion correcto",
+    ERROR: "Usuario o contraseña mal ingresado, verifique datos"
+}
+
+// FUNCION 
+// export const mensajeSalida = (code, message, data) => 
+//     ({
+//         SUCCESS: { code: `${code}.000`, message, data },
+//         ERROR: { code: `${code}.001`, message, data }
+//     });
