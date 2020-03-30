2332d2409c9d7ca9ee88ce0b889252d6b097d75b
Juan Carlos Venegas Zurita <juan.venegas1992@gmail.com>
2020-03-30 14:19:51 -0300

    se agrega / en variable de entorno para la versión

--- src/enviroment/env_config.js
+++ src/enviroment/env_config.js
@@ -5,9 +5,9 @@ export const BASE = process.env.BASE || 'api';
 // REGISTRO BILLER
 export const MS_REGISTROBILLER_URL = process.env.MS_REGISTROBILLER_URL || 'http://se-ms-registrobiller';
 export const MS_REGISTROBILLER_BASE = process.env.MS_REGISTROBILLER_BASE || '/ms/se-ms-registrobiller';
-export const MS_REGISTROBILLER_VERSION = process.env.MS_REGISTROBILLER_VERSION || 'v1';
+export const MS_REGISTROBILLER_VERSION = process.env.MS_REGISTROBILLER_VERSION || '/v1';
 
 // EMAIL
 export const MS_EMAIL_URL = process.env.MS_EMAIL_URL || 'http://se-ms-email';
 export const MS_EMAIL_BASE = process.env.MS_EMAIL_BASE || '/ms/se-ms-email';
-export const MS_EMAIL_VERSION = process.env.MS_EMAIL_VERSION || 'v1';
+export const MS_EMAIL_VERSION = process.env.MS_EMAIL_VERSION || '/v1';
