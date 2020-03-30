a7111de215c7ef67912b5de7cb4ccea81b0ce59c
Juan Carlos Venegas Zurita <juan.venegas1992@gmail.com>
2020-03-30 15:22:39 -0300

    soluciones varias de variables de entornos

--- src/app/services/v1/maintainer-user.service.js
+++ src/app/services/v1/maintainer-user.service.js
@@ -1,21 +1,17 @@
 import axios from "axios";
-import {
-  MS_REGISTROBILLER_URL,
-  MS_REGISTROBILLER_BASE,
-  MS_REGISTROBILLER_VERSION
-} from "../../../enviroment/env_config";
+import { MS_REGISTROBILLER_API_URL } from "../../../enviroment/env_config";
 
 export const registerNewUser = dataIn => {
-  const url = `${MS_REGISTROBILLER_URL}${MS_REGISTROBILLER_BASE}${MS_REGISTROBILLER_VERSION}/maintainerUser/registerNewUser`;
+  const url = `${MS_REGISTROBILLER_API_URL}/maintainerUser/registerNewUser`;
   return axios.post(url, data);
 };
 
 export const personalInformation = dataIn => {
-  const url = `${MS_REGISTROBILLER_URL}${MS_REGISTROBILLER_BASE}${MS_REGISTROBILLER_VERSION}/maintainer-user/informacion-personal`;
+  const url = `${MS_REGISTROBILLER_API_URL}/maintainer-user/informacion-personal`;
   return axios.post(url, dataIn);
 };
 
 export const updatePerfilUser = dataIn => {
-  const url = `${MS_REGISTROBILLER_URL}${MS_REGISTROBILLER_BASE}${MS_REGISTROBILLER_VERSION}/maintainer-user/editar-perfil`;
+  const url = `${MS_REGISTROBILLER_API_URL}/maintainer-user/editar-perfil`;
   return axios.put(url, dataIn);
 };
--- src/enviroment/env_config.js
+++ src/enviroment/env_config.js
@@ -2,12 +2,23 @@ export const URL = process.env.URL || 'http://se-bff-empresas';
 export const PORT = process.env.PORT || 8080;
 export const BASE = process.env.BASE || 'api';
 
+// INI VARIABLES DE ENTORNO
+export const EMAIL_SOURCE_EMAIL_VERIFIED = process.env.EMAIL_SOURCE_EMAIL_VERIFIED || 'michael.ordonez@siigroup.cl';
+export const EMAIL_TEMP_NAME_CLAVE_PROVISORIA = process.env.EMAIL_TEMP_NAME_CLAVE_PROVISORIA || 'seClaveProvisoria';
+export const EMAIL_CONFIGURATION_NAME = process.env.EMAIL_CONFIGURATION_NAME || 'TestConfigEmail';
+export const EMAIL_CONFIGURATION_API_URL = `${EMAIL_SOURCE_EMAIL_VERIFIED}/${EMAIL_TEMP_NAME_CLAVE_PROVISORIA}/${EMAIL_CONFIGURATION_NAME}`;
+// FIN VARIABLES DE ENTORNO
+
+// INI VARIABLES DE ENTORNO MS
 // REGISTRO BILLER
 export const MS_REGISTROBILLER_URL = process.env.MS_REGISTROBILLER_URL || 'http://se-ms-registrobiller';
-export const MS_REGISTROBILLER_BASE = process.env.MS_REGISTROBILLER_BASE || '/ms/se-ms-registrobiller';
+export const MS_REGISTROBILLER_BASE = process.env.MS_REGISTROBILLER_BASE || 'ms/se-ms-registrobiller';
 export const MS_REGISTROBILLER_VERSION = process.env.MS_REGISTROBILLER_VERSION || 'v1';
+export const MS_REGISTROBILLER_API_URL = `${MS_REGISTROBILLER_URL}/${MS_REGISTROBILLER_BASE}/${MS_REGISTROBILLER_VERSION}`;
 
 // EMAIL
 export const MS_EMAIL_URL = process.env.MS_EMAIL_URL || 'http://se-ms-email';
-export const MS_EMAIL_BASE = process.env.MS_EMAIL_BASE || '/ms/se-ms-email';
+export const MS_EMAIL_BASE = process.env.MS_EMAIL_BASE || 'ms/se-ms-email';
 export const MS_EMAIL_VERSION = process.env.MS_EMAIL_VERSION || 'v1';
+export const MS_EMAIL_API_URL = `${MS_EMAIL_URL}/${MS_EMAIL_BASE}/${MS_EMAIL_VERSION}`;
+// FIN VARIABLES DE ENTORNO MS
