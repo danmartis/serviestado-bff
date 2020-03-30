558f8347590b46f544c82663b23956dbce83cd14
Felipe Godoy C <fgodoy6@externos.bancoestado.cl>
2020-03-30 12:45:37 -0300

    fix env config

--- src/enviroment/env_config.js
+++ src/enviroment/env_config.js
@@ -14,6 +14,6 @@ export const MS_EMAIL_VERSION = process.env.MS_EMAIL_VERSION || 'v1';
 
 // CONVENIOS Y RECAUDACION
 export const MS_RECAUDACION_URL = process.env.MS_RECAUDACION_URL || 'urlaws'
-export const MS_RECAUDACION_BASE = process.env.MS_RECAUDACION_BASE || 'ms/se-ms-recaudacion/v1/';
+export const MS_RECAUDACION_BASE = process.env.MS_RECAUDACION_BASE || 'ms/se-ms-recaudacion';
 export const MS_RECAUDACION_VERSION = process.env.MS_RECAUDACION_VERSION || 'v1';
 export const MS_RECAUDACION_API_URL = `${MS_RECAUDACION_URL}/${MS_RECAUDACION_BASE}/${MS_RECAUDACION_VERSION}`;
