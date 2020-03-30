a86d7e58eeccf1e9cf271c3350985ecbaf822609
Felipe Godoy C <fgodoy6@externos.bancoestado.cl>
2020-03-30 12:06:06 -0300

    configuracion inicial env

---
+++ src/app/services/v1/convenios.service.js
@@ -0,0 +1,6 @@
+import axios from "axios";
+import { MS_RECAUDACION_URL, MS_RECAUDACION_BASE, MS_RECAUDACION_VERSION } from "../../../enviroment/env_config";
+
+const url = `${MS_RECAUDACION_URL}/${MS_RECAUDACION_BASE}/${MS_RECAUDACION_VERSION}`;
+
+export const 
