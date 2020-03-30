bcee056a8bf91cfef78370384ea131a56e9815eb
Felipe Godoy C <fgodoy6@externos.bancoestado.cl>
2020-03-30 12:46:39 -0300

    fix

--- src/app/services/v1/recaudacion.service.js
+++ src/app/services/v1/recaudacion.service.js
@@ -3,6 +3,6 @@ import { MS_RECAUDACION_API_URL } from "../../../enviroment/env_config";
 
 
 export const getConvenioByEmpresa = (idConvenio) => {
-  const uri = `${MS_RECAUDACION_API_URL}/recaudacion/${idConvenio}`;
+  const uri = `${MS_RECAUDACION_API_URL}/recaudacion/${idConvenio}`; 
   return axios.get(uri);
 }
