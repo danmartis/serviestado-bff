954828f907b251a567fdd68f6181766416af48af
Felipe Godoy C <fgodoy6@externos.bancoestado.cl>
2020-03-30 16:18:46 -0300

    fix lint

--- src/app/controllers/v1/recaudacion.controller.js
+++ src/app/controllers/v1/recaudacion.controller.js
@@ -2,15 +2,14 @@ import { getRecaudacionByConvenio } from '../../services/v1/recaudacion.service'
 import { CODE_RESP_OK, GET_RECAUDACION_CONVENIO, CODE_MESSAGE_OK, CODE_RESP_BAD_REQUEST, mensajeSalida } from '../../utils/mensaje-salida.service';
 
 export const getRecaudacionByConvenioController = (req, res) => {
-    console.log("Params: ", req.params)
-    const idConvenio = req.params.idConvenio;
-    getRecaudacionByConvenio(idConvenio)
-      .then( recaudacion =>
-        res.status(CODE_RESP_OK).json(mensajeSalida(CODE_MESSAGE_OK, GET_RECAUDACION_CONVENIO.SUCCESS, { ...recaudacion.data.data })))
-      .catch( err => 
-        res.status(CODE_RESP_BAD_REQUEST).json(
-          mensajeSalida(CODE_MESSAGE_ERROR, GET_RECAUDACION_CONVENIO.ERROR, {
-            ...err.response.data
-          })))
+  const idConvenio = req.params.idConvenio;
+  getRecaudacionByConvenio(idConvenio)
+    .then( recaudacion =>
+      res.status(CODE_RESP_OK).json(mensajeSalida(CODE_MESSAGE_OK, GET_RECAUDACION_CONVENIO.SUCCESS, { ...recaudacion.data.data })))
+    .catch( err => 
+      res.status(CODE_RESP_BAD_REQUEST).json(
+        mensajeSalida(CODE_MESSAGE_ERROR, GET_RECAUDACION_CONVENIO.ERROR, {
+          ...err.response.data
+        })))
 
 }
